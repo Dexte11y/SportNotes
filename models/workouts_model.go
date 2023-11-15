9e02df1b10e1aa2e2e8b4bd96eb06941243cc6ba
Daniil <d.federyakin@inbox.ru>
2023-11-15 03:37:23 +0300

    Реализован CRUD для аккаунтов

--- models/workouts_model.go
+++ models/workouts_model.go
@@ -8,12 +8,3 @@ type Workouts struct {
 	UserId int       `gorm:"type:int"`
 	Date   time.Time `gorm:"type:date"`
 }
-
-// Модель полей для таблицы users
-type Users struct {
-	Id       int    `gorm:"type:int"`
-	Name     string `gorm:"type:varchar(255)"`
-	LastName string `gorm:"type:varchar(255)"`
-	Email    string `gorm:"type:varchar(255)"`
-	Password string `gorm:"type:varchar(255)"`
-}
